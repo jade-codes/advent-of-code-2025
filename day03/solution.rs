e90f0a5b07cd7d1f6248df7e0991a209759a1759
jade-codes <jade_wilson66@hotmail.co.uk>
2025-12-03 10:22:34 +0000

    Add day 3

--- day03/solution.rs
+++ day03/solution.rs
@@ -4,14 +4,74 @@ use std::path::Path;
 #[path = "../utils.rs"]
 mod utils;
 
-fn part1(_lines: &[String]) -> i64 {
-    // TODO: Implement part 1
-    0
+fn part1(lines: &[String]) -> i64 {
+    solve_batteries(lines, 2)
 }
 
-fn part2(_lines: &[String]) -> i64 {
-    // TODO: Implement part 2
-    0
+fn part2(lines: &[String]) -> i64 {
+    solve_batteries(lines, 12)
+}
+
+fn solve_batteries(lines: &[String], target_count: usize) -> i64 {
+    let mut max_total = 0;
+
+    for line in lines {
+        let numbers: Vec<i64> = line
+            .chars()
+            .filter(|c| c.is_ascii_digit())
+            .map(|c| c.to_digit(10).unwrap() as i64)
+            .collect();
+        
+        let n = numbers.len();
+        let mut dp: Vec<Vec<Option<Vec<i64>>>> = vec![vec![None; target_count + 1]; n + 1];
+        dp[0][0] = Some(vec![]);
+        
+        for i in 0..n {
+            for j in 0..=target_count.min(i + 1) {
+                if dp[i][j].is_none() {
+                    continue;
+                }
+                
+                let curr_sequence = dp[i][j].as_ref().unwrap().clone();
+                
+                // Option 1: Don't take this battery
+                if i + 1 < n || j == target_count {
+                    if dp[i + 1][j].is_none() || is_better(&curr_sequence, dp[i + 1][j].as_ref().unwrap()) {
+                        dp[i + 1][j] = Some(curr_sequence.clone());
+                    }
+                }
+                
+                // Option 2: Take this battery
+                if j < target_count {
+                    let mut next_sequence = curr_sequence.clone();
+                    next_sequence.push(numbers[i]);
+                    if dp[i + 1][j + 1].is_none() || is_better(&next_sequence, dp[i + 1][j + 1].as_ref().unwrap()) {
+                        dp[i + 1][j + 1] = Some(next_sequence);
+                    }
+                }
+            }
+        }
+        
+        // Find the best with exactly target_count batteries
+        let result_digits = dp[n][target_count].as_ref().unwrap();
+        let mut result = 0_i64;
+        for &digit in result_digits {
+            result = result * 10 + digit;
+        }
+        max_total += result;
+    }
+    max_total
+}
+
+fn is_better(a: &[i64], b: &[i64]) -> bool {
+    for i in 0..a.len().min(b.len()) {
+        if a[i] > b[i] {
+            return true;
+        } else if a[i] < b[i] {
+            return false;
+        }
+    }
+    a.len() > b.len()
 }
 
 pub fn main() {
